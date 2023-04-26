1f321bf38f363aec18685b1ca77d5d7d6de400e4
linjh <516308181@qq.com>
2023-04-26 12:44:58 +0800

    01-全局配置请求控制

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { VersioningType, VERSION_NEUTRAL } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import {
+  NestFastifyApplication,
+  FastifyAdapter,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  // 接口版本化管理
+  app.enableVersioning({
+    defaultVersion: [VERSION_NEUTRAL, '1', '2'],
+    type: VersioningType.URI,
+  });
+  await app.listen(3000);
+}
+bootstrap();
