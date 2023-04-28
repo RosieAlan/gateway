112734840e947930768a59949b25638cba047ff8
linjh <516308181@qq.com>
2023-04-28 14:12:11 +0800

    03-使用自定义配置

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
+import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -20,6 +22,7 @@ async function bootstrap() {
   });
   //统一响应体格式
   app.useGlobalInterceptors(new TransformInterceptor)
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,25 +7,33 @@ import {
   Param,
   Delete,
   Version,
+  VERSION_NEUTRAL,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { BusinessException } from 'src/common/exceptions/business.exception';
+import { ConfigService } from '@nestjs/config';
 @Controller({
   path: 'user',
   version: '1',
 })
 export class UserController {
-  constructor(private readonly userService: UserService) {}
-
+  constructor(
+    private readonly userService: UserService,
+    private readonly configService: ConfigService,
+  ) {}
+  @Get('getTestName')
+  getTestName() {
+    return this.configService.get('TEST_VALUE').name;
+  }
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
   @Get()
-  @Version('1')
+  @Version([VERSION_NEUTRAL, '1'])
   findAll() {
     return this.userService.findAll();
   }
@@ -38,7 +46,24 @@ export class UserController {
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
-
+  @Get('findError')
+  @Version([VERSION_NEUTRAL, '1'])
+  findError() {
+    const a: any = {};
+    console.log(a.b.c);
+    return this.userService.findAll();
+  }
+  @Get('findBusinessError')
+  @Version([VERSION_NEUTRAL, '1'])
+  findBusinessError() {
+    const a: any = {};
+    try {
+      console.log(a.b.c);
+    } catch (error) {
+      throw new BusinessException('你这个参数错了');
+    }
+    return this.userService.findAll();
+  }
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
