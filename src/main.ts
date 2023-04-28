da192bb95f00a08f56b990b76cab28a877e7e7b0
linjh <516308181@qq.com>
2023-04-28 14:27:27 +0800

    05-Swagger 文档

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
 import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
+import { generateDocument } from './doc';
 declare const module: any;
 async function bootstrap() {
   if (module.hot) {
@@ -24,6 +25,8 @@ async function bootstrap() {
     defaultVersion: [VERSION_NEUTRAL, '1', '2'],
     type: VersioningType.URI,
   });
+  // 创建文档
+  generateDocument(app);
   //统一响应体格式
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
